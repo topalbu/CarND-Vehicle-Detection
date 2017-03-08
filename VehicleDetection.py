836c8ef3f778f597e0408d4c19dbf9637ab82097
topalbu <buraktopal53@gmail.com>
2017-03-08 07:09:04 +0300

    VehicleDetector class implemented. It use heat and age attributes of the Box class to get rid of the false positives.

--- VehicleDetection.py
+++ VehicleDetection.py
@@ -5,79 +5,125 @@ from lesson_functions import *
 import pickle
 from scipy.ndimage.measurements import label
 
+
 class Box:
+    # Box class to represent and keep track of the cars on a video stream
     def __init__(self, box):
+        # x1 coordinate of the box
         self.startX = box[0][0]
+        # y1 coordinate of the box
         self.startY = box[0][1]
+        # x2 coordinate of the box
         self.endX = box[1][0]
+        # y2 coordinate of the box
         self.endY = box[1][1]
+        # center (x,y) coordinte
         self.center = self.calculate_center()
+        # width (x2-x1) length
         self.width = self.calculate_width()
-        self.height =self.calculate_height()
-        self.heat = 0 # #
-        self.speed = [0,0] # speed vector in xy pixel coordinates
+        # height (y2-y1) length
+        self.height = self.calculate_height()
+        # heat of the box (represent
+        self.heat = 0
+        # speed vector in x-y pixel coordinates
+        self.speed = [0, 0]
+        # age to track and get rid of old boxes (vehicles detected and dragged along the video frames)
         self.age = 0
 
-    def __delitem__(self, key):
-        self.__delattr__(key)
-
     def __getitem__(self, key):
+        """
+            Return the value of start or end points of the box
+            :param key to select start or end point
+        """
         if key == 0:
             return (self.__getattribute__('startX'), self.__getattribute__('startY'))
         elif key == 1:
             return (self.__getattribute__('endX'), self.__getattribute__('endY'))
 
     def calculate_center(self):
+        """
+            Calculates the center coordinates of the box
+        """
         return [(self.startX + self.endX) / 2., (self.startY + self.endY) / 2.]
 
     def calculate_distance(self, other):
+        """
+            Calculates center to center distance to given box
+            :param other box to calculate distance
+        """
         return math.sqrt((self.center[0] - other.center[0]) ** 2 + (self.center[1] - other.center[1]) ** 2)
 
-    def isEqual(self, other):
+    def is_equal(self, other):
+        """
+            Checks if the given box has similar attributes
+            :param other box to compare
+        """
         return self.close(other) & (abs(self.width - other.width) < 100) & (abs(self.height - other.height) < 100)
 
     def close(self, other):
+        """
+            Checks if the given box is closer in a certain threhold
+            :param other box to compare
+        """
         return (self.calculate_distance(other) < 200)
 
     def calculate_width(self):
+        """
+            Calculates width of the box
+        """
         return self.endX - self.startX
 
     def calculate_height(self):
+        """
+            Calculates height of the box
+        """
         return self.endY - self.startY
 
-    def printCords(self):
+    def print_cords(self):
+        """
+            Prints the x and y coorinates of the box
+        """
         print('startX  :', self.startX, ' ,startY  :', self.startY, ' ,endX  :', self.endX, ' ,endY  :', self.endY)
 
-    def merge(self, other):
-        # new_center = [0,0]
-        # new_center[0] = (self.center[0]*self.heat + other.center[0]*other.heat)/2.
-        # new_center[1] = (self.center[1]*self.heat + other.center[1]*other.heat)/2.
-        sum_heat = (self.heat + other.heat)
-        self.startX = int((self.startX * self.heat + other.startX * other.heat) / (sum_heat * 1.01))
-        self.startY = int((self.startY * self.heat + other.startY * other.heat) / (sum_heat * 1.01))
-        self.endX = int((self.endX * self.heat + other.endX * other.heat) / (sum_heat * 0.99))
-        self.endY = int((self.endY * self.heat + other.endY * other.heat) / (sum_heat * 0.99))
-        self.center = self.calculate_center()
-        self.width = self.endX - self.startX
-        self.height = self.endY - self.startY
-        self.heat = sum_heat / 2.
-        del other
+    # def merge(self, other):
+    #     # new_center = [0,0]
+    #     # new_center[0] = (self.center[0]*self.heat + other.center[0]*other.heat)/2.
+    #     # new_center[1] = (self.center[1]*self.heat + other.center[1]*other.heat)/2.
+    #     sum_heat = (self.heat + other.heat)
+    #     self.startX = int((self.startX * self.heat + other.startX * other.heat) / (sum_heat * 1.01))
+    #     self.startY = int((self.startY * self.heat + other.startY * other.heat) / (sum_heat * 1.01))
+    #     self.endX = int((self.endX * self.heat + other.endX * other.heat) / (sum_heat * 0.99))
+    #     self.endY = int((self.endY * self.heat + other.endY * other.heat) / (sum_heat * 0.99))
+    #     self.center = self.calculate_center()
+    #     self.width = self.endX - self.startX
+    #     self.height = self.endY - self.startY
+    #     self.heat = sum_heat / 2.
+    #     del other
 
-    def calculate_speed(self,old):
+
+    def calculate_speed(self, old):
+        """
+            Calculates the speed in xy pixel coordinate wtih respect to old position
+            :param old box from the previous frame
+        """
         self.speed[0] = self.center[0] - old.center[0]
         self.speed[1] = self.center[1] - old.center[1]
 
     def _print(self):
-        print('center  :', self.center, ' widht : ', self.width, ' height : ', self.height, ' heat : ', self.heat , ' speed ' , self.speed)
+        """
+            Prints the attributes of the box
+        """
+        print('center  :', self.center, ' widht : ', self.width, ' height : ', self.height, ' heat : ', self.heat,
+              ' speed ', self.speed)
 
 
 # Define a function you will pass an image
 # and the list of windows to be searched (output of slide_windows())
-def search_windows(img, windows, clf, scaler, color_space='RGB',
-                   spatial_size=(32, 32), hist_bins=32,
-                   hist_range=(0, 256), orient=9,
+def search_windows(img, windows, clf, scaler, color_space='YCrCb',
+                   spatial_size=(16, 16), hist_bins=16,
+                   orient=8,
                    pix_per_cell=8, cell_per_block=2,
-                   hog_channel=0, spatial_feat=True,
+                   hog_channel='ALL', spatial_feat=True,
                    hist_feat=True, hog_feat=True):
     # 1) Create an empty list to receive positive detection windows
     on_windows = []
@@ -106,9 +152,6 @@ def search_windows(img, windows, clf, scaler, color_space='RGB',
     # Define a function to draw bounding boxes
 
 
-
-
-
 class VehicleDetector:
     old_positions = []  # positions of detectefd vehicles on the last frame
     car_list = []
@@ -117,7 +160,8 @@ class VehicleDetector:
     X_scaler = None
     parameters = None
     debug = True
-    image_shape = (720,1280)
+    image_shape = (720, 1280)
+
     def __init__(self, image):
         self.heat = np.zeros_like(image[:, :, 0]).astype(np.float)
         self.image = image
@@ -132,6 +176,8 @@ class VehicleDetector:
             self.X_scaler = model['X_scaler']
             self.parameters = model['parameters']
 
+        print(self.parameters)
+
     def age_heat_map(self):
         nonzero = self.heat.nonzero()
         self.heat[nonzero] -= 1
@@ -140,7 +186,7 @@ class VehicleDetector:
         index = 1
         x_q = int(self.image.shape[1] / 4)
         # split x into 3 region 0:width/4 , withd/4: 3*width/4 , 3*width/4:width
-        """""
+        """
         for startx in range(0, self.image.shape[1], x_q):
             self.search_windows += slide_window(self.image, x_start_stop=[startx, startx + x_q],
                                                 y_start_stop=[400, 500],
@@ -156,7 +202,7 @@ class VehicleDetector:
                                                 xy_window=(160 * int(index / 2), 160), xy_overlap=(0.5, 0.5))
             index += 1
         """
-        self.search_windows += slide_window(self.image.shape, x_start_stop=[0,image.shape[1]],
+        self.search_windows += slide_window(self.image.shape, x_start_stop=[0, image.shape[1]],
                                             y_start_stop=[400, 500],
                                             xy_window=(80, 80), xy_overlap=(0.75, 0.75))
         self.search_windows += slide_window(self.image.shape, x_start_stop=[None, None],
@@ -168,11 +214,13 @@ class VehicleDetector:
         self.search_windows += slide_window(self.image.shape, x_start_stop=[None, None],
                                             y_start_stop=[500, 690],
                                             xy_window=(160, 160), xy_overlap=(0.5, 0.5))
+
     def create_search_windows2(self):
         # split x into 3 region 0:width/4 , withd/4: 3*width/4 , 3*width/4:width
-        self.search_windows = slide_window(self.image, x_start_stop=[0,1200],
-                                            y_start_stop=[400, 500],
-                                            xy_window=(80, 80), xy_overlap=(0.75, 0.75))
+        self.search_windows = slide_window(self.image, x_start_stop=[0, 1200],
+                                           y_start_stop=[400, 500],
+                                           xy_window=(80, 80), xy_overlap=(0.75, 0.75))
+
     def create_heatmap(self, bbox_list):
         heatmap = np.zeros_like(self.image[:, :, 0]).astype(np.float)
         for box in bbox_list:
@@ -205,10 +253,10 @@ class VehicleDetector:
             bbox.heat = self.heat[bbox.center[1]][bbox.center[0]]
 
             for pos in self.old_positions:
-                if pos.isEqual(bbox):
+                if pos.is_equal(bbox):
                     self.old_positions.remove(pos)
                     bbox.merge(pos)
-                    bbox.heat+=1
+                    bbox.heat += 1
 
             self.car_list.append(bbox)
             # for pos in car_list:
@@ -242,6 +290,7 @@ class VehicleDetector:
         draw_image = draw_boxes(image, self.car_list)
         return draw_image
 
+
 image = mpimg.imread('test_images/test1.jpg')
 
 vehicleDetertor = VehicleDetector(image)
@@ -265,6 +314,7 @@ def apply_threshold(heatmap, threshold):
     # Return thresholded map
     return heatmap
 
+
 def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
     # Make a copy of the image
     imcopy = np.copy(img)
@@ -276,13 +326,14 @@ def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
             cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
             font = cv2.FONT_HERSHEY_SIMPLEX
             speed_text = "Heat: {0:.2f} ".format(bbox.heat)
-            cv2.putText(imcopy, speed_text, (int(bbox.center[0]),int(bbox.center[1])), font, 1, (255, 255, 255), 2)
-            #speed_text = "Y Speed: {0:.2f} ".format(bbox.speed[1])
-            #cv2.putText(imcopy, speed_text, (int(bbox.center[0]),int(bbox.center[1]+20)), font, 1, (255, 255, 255), 2)
+            cv2.putText(imcopy, speed_text, (int(bbox.center[0]), int(bbox.center[1])), font, 1, (255, 255, 255), 2)
+            # speed_text = "Y Speed: {0:.2f} ".format(bbox.speed[1])
+            # cv2.putText(imcopy, speed_text, (int(bbox.center[0]),int(bbox.center[1]+20)), font, 1, (255, 255, 255), 2)
     # Return the image copy with boxes drawn
     return imcopy
 
-def draw_labeled_bboxes(img,labels):
+
+def draw_labeled_bboxes(img, labels):
     # Iterate through all detected cars
     for car_number in range(1, labels[1] + 1):
         # Find pixels with each car_number label value
@@ -297,11 +348,12 @@ def draw_labeled_bboxes(img,labels):
     # Return the image
     return img
 
+
 def locate_cars(img):
     # Iterate through all detected cars
     # Find final boxes from heatmap using label function
-    #add_heat(img,vehicleDetertor.old_positions)
-    #apply_threshold(img,len(vehicleDetertor.old_positions)-1)
+    # add_heat(img,vehicleDetertor.old_positions)
+    # apply_threshold(img,len(vehicleDetertor.old_positions)-1)
 
 
     labels = label(img)
@@ -315,38 +367,62 @@ def locate_cars(img):
         # Define a bounding box based on min/max x and y
         bbox = Box(((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy))))
         bbox.heat = img[bbox.center[1]][bbox.center[0]]
-        #if bbox.heat > 5:
+        # if bbox.heat > 5:
         car_list.append(bbox)
 
     # Return car list
 
     for old_car in vehicleDetertor.old_positions:
-        #print('old cars : ')
-        #old_car._print()
-        #print()
+        # print('old cars : ')
+        # old_car._print()
+        # print()
         found = False
         for car in car_list:
-            #print('new cars : ')
-            #car._print()
-            if car.isEqual(old_car):
-                #print('is  equal : ')
+            # print('new cars : ')
+            # car._print()
+            if car.is_equal(old_car):
+                # print('is  equal : ')
                 car.calculate_speed(old_car)
-                car.heat = max(car.heat,old_car.heat) + 1
+                car.heat = max(car.heat, old_car.heat) + 1
                 found = True
                 break
 
         if not found and old_car.heat > 3 and old_car.age < 4:
-            #print(' old car not found in the new frame but shoud be there :   ' ,old_car.heat)
-            old_car.age +=1
-            old_car.heat -=1
+            # print(' old car not found in the new frame but shoud be there :   ' ,old_car.heat)
+            old_car.age += 1
+            old_car.heat -= 1
             car_list.append(old_car)
 
-        #print()
+            # print()
     return car_list
 
+from multiprocessing import Pool
+pool = Pool(4)
 def process_image(image):
     copy_image = np.copy(image)
     copy_image = copy_image.astype(np.float32) / 255.
+
+    A = (copy_image, vehicleDetertor.search_windows[0:50], vehicleDetertor.svc,
+                                 vehicleDetertor.X_scaler)
+    B = (copy_image, vehicleDetertor.search_windows[50:100], vehicleDetertor.svc,
+                                 vehicleDetertor.X_scaler)
+
+    C = (copy_image, vehicleDetertor.search_windows[100:150], vehicleDetertor.svc,
+         vehicleDetertor.X_scaler)
+
+    D = (copy_image, vehicleDetertor.search_windows[150:len(vehicleDetertor.search_windows)], vehicleDetertor.svc,
+         vehicleDetertor.X_scaler)
+
+    result1 = pool.apply_async(search_windows, A)  # evaluate "solve1(A)" asynchronously
+    result2 = pool.apply_async(search_windows, B)  # evaluate "solve2(B)" asynchronously
+    result3 = pool.apply_async(search_windows, C)  # evaluate "solve1(A)" asynchronously
+    result4 = pool.apply_async(search_windows, D)  # evaluate "solve2(B)" asynchronously
+    answer1 = result1.get(timeout=10)
+    answer2 = result2.get(timeout=10)
+    answer3 = result3.get(timeout=10)
+    answer4 = result4.get(timeout=10)
+
+    '''
     hot_windows = search_windows(copy_image, vehicleDetertor.search_windows, vehicleDetertor.svc,
                                  vehicleDetertor.X_scaler, color_space=vehicleDetertor.parameters['color_space'],
                                  spatial_size=vehicleDetertor.parameters['spatial_size'],
@@ -358,28 +434,34 @@ def process_image(image):
                                  spatial_feat=vehicleDetertor.parameters['spatial_feat'],
                                  hist_feat=vehicleDetertor.parameters['hist_feat'],
                                  hog_feat=vehicleDetertor.parameters['hog_feat'])
+    '''
+    hot_windows = answer1
+    hot_windows += answer2
+    hot_windows += answer3
+    hot_windows += answer4
     heat = np.zeros_like(image[:, :, 0]).astype(np.float)
-    add_heat(heat,hot_windows)
-    apply_threshold(heat,1)
+    add_heat(heat, hot_windows)
+    apply_threshold(heat, 1)
     # Visualize the heatmap when displaying
-    #heatmap = np.clip(heat, 0, 255)
+    # heatmap = np.clip(heat, 0, 255)
 
     VehicleDetector.old_positions = vehicleDetertor.car_list
     vehicleDetertor.car_list = locate_cars(heat)
 
-    #labels = label(heat)
-    #draw_img = draw_labeled_bboxes(image,labels)
-    draw_img = draw_boxes(image,vehicleDetertor.car_list)
+    # labels = label(heat)
+    # draw_img = draw_labeled_bboxes(image,labels)
+    draw_img = draw_boxes(image, vehicleDetertor.car_list)
     return draw_img
 
 
 import os
 from moviepy.editor import VideoFileClip
-white_output = 'white.mp4' # New video
+
+white_output = 'white.mp4'  # New video
 os.remove(white_output)
-clip1 = VideoFileClip('project_video.mp4')#.subclip(21.00,25.00) # project video
-#clip = VideoFileClip("myHolidays.mp4", audio=True).subclip(50,60)
-white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
+clip1 = VideoFileClip('project_video.mp4')  # .subclip(21.00,25.00) # project video
+# clip = VideoFileClip("myHolidays.mp4", audio=True).subclip(50,60)
+white_clip = clip1.fl_image(process_image)  # NOTE: this function expects color images!!
 white_clip.write_videofile(white_output, audio=False)
 
 # images = glob.glob('test_images/*.jpg')
@@ -401,4 +483,3 @@ white_clip.write_videofile(white_output, audio=False)
 #     plt.title('Heat Map')
 #     fig.tight_layout()
 #     plt.show()
-
